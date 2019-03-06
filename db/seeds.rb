245824c204d04279cf2e98864ac6a329ac08234c
nicolas lequeux <contact@nicolaslequeux.com>
2019-03-06 15:43:44 +0100

    mid-day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '012345678',
+    category:     'french'
+  },
+  {
+    name:         'Bounty',
+    address:      'Rue de la Paix, 75008 Paris',
+    phone_number: '"3877345678',
+    category:     'french'
+  },
+  {
+    name:         'Cacio & Pepe',
+    address:      'Naviglio, Milan',
+    phone_number: '"000345678',
+    category:     'italian'
+  },
+  {
+    name:         'New China',
+    address:      'Bejing',
+    phone_number: '"000345678',
+    category:     'chinese'
+  },
+  {
+    name:         'Mc Pepe',
+    address:      'Napoli',
+    phone_number: '"000345678',
+    category:     'italian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
